54aca4ed9ee6088bd646eb64c20c479c881df089
c_hello_world <c_hello_world@163.com>
2016-11-04 08:15:58 +0800

    11.04 xiehongxing

--- src/fdfs/fdfs_client.c
+++ src/fdfs/fdfs_client.c
@@ -1,6 +1,21 @@
+/************************************************************************
+ *  作者:       谢红星                                                  *
+ *  日期:       2016/11/3                                               *
+ *  描述:       该文件代码对客户端进行初始化，销毁客户端,断开与服务器   *
+ *              的连接；                                                *
+ ************************************************************************/
+
 #include <fdfs_head.h>
 
 
+/**********************************************************
+ * 方法:    op_fdfs_client_init                           *
+ * 参数:    pTrackerServer:   --    返回tracker的连接     *
+ *          conf_filename:    --    配置文件              *
+ * 返回:    retn               0    成功                  *
+ *                             1    失败                  *
+ *                                                        *
+ **********************************************************/
 int op_fdfs_client_init(ConnectionInfo **pTrackerServer, const char * conf_filename)
 {
     int retn = 0;
@@ -26,6 +41,14 @@ END:
     return retn;
 }
 
+
+
+/**********************************************************
+ * 方法:    op_fdfs_client_destroy                        *
+ * 参数:    ConnectionInfo   --    tracker的连接          *
+ * 返回:    retn               0    成功                  *
+ * 描述：   销毁客户端对tracker的连接；                   *
+ **********************************************************/
 int op_fdfs_client_destroy(ConnectionInfo *pTrackerServer)
 {
     int retn = 0;
@@ -36,6 +59,13 @@ int op_fdfs_client_destroy(ConnectionInfo *pTrackerServer)
     return retn;
 }
 
+
+/**********************************************************
+ * 方法:    op_fdfs_disconnect_server                     *
+ * 参数:    conn        tracker/storage的连接             *
+ * 返回:    retn        0    成功                         *
+ * 描述：   销毁客户端对tracker的连接；                   *
+ **********************************************************/
 int op_fdfs_disconnect_server(ConnectionInfo *conn)
 {
     int retn = 0;
@@ -44,13 +74,5 @@ int op_fdfs_disconnect_server(ConnectionInfo *conn)
 
     return retn;
 }
-
-
-
-
-
-
-
-
 
 
